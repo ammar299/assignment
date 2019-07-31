b7c7041b26113720df1d4b16924b9557ab2f4ccd
ammar299 <ammynice0@gmail.com>
2019-07-31 05:38:39 +0500

    assignment done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
 	resources :articles
 	resources :users, except: [:new]
   root 'pages#index'
+  get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
