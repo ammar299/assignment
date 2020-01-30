2f48fa93bfc7505e26cca96ffbab504212285b25
Rana Amir <ramir2426@gmail.com>
2020-01-30 06:15:10 +0500

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_30_213108) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
